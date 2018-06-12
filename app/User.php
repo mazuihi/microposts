ccfd4c2d42477644fd7994f875c73573fb394a6f
ichiro.kirameki <ec2-user@ip-172-31-29-145.us-east-2.compute.internal>
2018-06-12 06:52:32 +0000

    Procfile

--- app/User.php
+++ app/User.php
@@ -31,11 +31,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Micropost::class);
     }
-        public function followings()
+    
+    public function followings()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
     }
-
     public function followers()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
@@ -70,4 +70,10 @@ class User extends Authenticatable
         return $this->followings()->where('follow_id', $userId)->exists();
 }
  
+     public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
 }
